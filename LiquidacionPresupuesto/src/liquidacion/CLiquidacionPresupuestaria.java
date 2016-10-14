61c7e15da7f4d2a7a3b1fd0b8899229d43741147
Rederz <lurendel@gmail.com>
2016-10-14 08:46:59 -0600

    Modificacion a la clase de liquidacion Pressupuestaria

--- LiquidacionPresupuesto/src/liquidacion/CLiquidacionPresupuestaria.java
+++ LiquidacionPresupuesto/src/liquidacion/CLiquidacionPresupuestaria.java
@@ -11,108 +11,109 @@ public class CLiquidacionPresupuestaria {
 
 	private static final String QUERY_GASTOS_MUNI = "select * from GASTO WHERE monto_renglon>0 AND fuente=29 ORDER BY fec_aprobado";
 	private static final String QUERY_GASTOS_FONDO = "select * from GASTO WHERE monto_renglon>0 AND fuente=11 ORDER BY fec_aprobado";
-	private static final String QUERY_pago = "select * from ISCV order by fecha_recaudo";
-	
-	private static CDatabase dbGastosMuni;
-	private static CDatabase dbGastosFondo;
-	private static CDatabase dbpago_munis;
-	
-	public static boolean liquidarPresupuesto(PrintWriter writer, boolean show_pagos) {
+	private static final String QUERY_PAGO = "select * from ISCV order by fecha_recaudo";
 
-		dbGastosMuni = new CDatabase();
-		dbGastosFondo = new CDatabase();
-		dbpago_munis = new CDatabase();
-		//dbLiquidacion = new Database();
+	private static final String INSERT_PAGO_GASTOS = "insert into ISCV_GASTO (formulario,documento,geografico_ingreso,no_cur,entidad,unidad_ejecutora,programa,subprograma,proyecto,actividad,obra,renglon,geografico_gasto,monto,tipo) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+	public static boolean liquidarPresupuesto(PrintWriter writer, boolean show_pagos) {
+		CDatabase dbGastosMuni = new CDatabase();
+		CDatabase dbGastosFondo = new CDatabase();
+		
+		CDatabase dbpago_munis = new CDatabase();
+
+		CDatabase dbpago_gastos = new CDatabase();
 
 		try {
 
-			if (dbGastosMuni.isOpen() && dbpago_munis.isOpen() && dbGastosFondo.isOpen()) {
+			if (dbGastosMuni.isOpen() && dbpago_munis.isOpen() && dbGastosFondo.isOpen() && dbpago_munis.isOpen()
+					&& dbpago_gastos.isOpen()) {
 
 				ResultSet resultGastosMuni = dbGastosMuni.runQuery(QUERY_GASTOS_MUNI);
 				ResultSet resultGastosFondo = dbGastosFondo.runQuery(QUERY_GASTOS_FONDO);
-				ResultSet resultpago = dbpago_munis.runQuery(QUERY_pago);
+				ResultSet resultpago = dbpago_munis.runQuery(QUERY_PAGO);
 
-				//Double total = 0.0;
+				dbpago_gastos.setSqlInsert(INSERT_PAGO_GASTOS);
+
 				Double saldo_muni = 0.0;
 				Double saldo_fondo = 0.0;
 				Double pago_muni = 0.0;
 				Double pago_fondo = 0.0;
 				Double gasto_muni = 0.0;
 				Double gasto_fondo = 0.0;
-				long cont=1;
-				while(resultpago.next()){
-					pago_muni = resultpago.getDouble("valor_pago")*0.5;
-					pago_fondo = resultpago.getDouble("valor_pago")*0.5;
-					writer.println("Contribuyente #"+cont+" con su aporte"+(show_pagos ? " de Q "+pago_muni : "")+" usted contribuyo a: ");
-					if(saldo_muni<0 && !resultGastosMuni.isAfterLast()){
-						writer.println(String.join("","     Entidad: ",resultGastosMuni.getString("entidad_nombre"),", ",resultGastosMuni.getString("unidad_ejecutora_nombre")));
-						writer.println(String.join("","     ",resultGastosMuni.getString("municipio_nombre"),", ",resultGastosMuni.getString("departamento_nombre")));
-						writer.println(String.join("","     ", resultGastosMuni.getString("renglon_nombre")+", Q ",gasto_muni.toString()));
+				long cont = 1;
+
+				writer.println("INCIA...");
+
+				while (resultpago.next()) {
+					pago_muni = resultpago.getDouble("valor_pago") * 0.5;
+					pago_fondo = resultpago.getDouble("valor_pago") * 0.5;
+
+					if (saldo_muni < 0 && !resultGastosMuni.isAfterLast()) {
+						Double temp_saldo = saldo_muni;
+
 						saldo_muni = saldo_muni + pago_muni;
-						if(show_pagos)
-							writer.println("saldo_muni: "+saldo_muni);
-						writer.println();
-					}
-					else
+
+						if (saldo_muni < 0) {
+							temp_saldo = pago_muni;
+						}
+
+						agregarIscvGasto(dbpago_gastos, resultpago, resultGastosMuni, temp_saldo, "MU");
+					} else
 						saldo_muni = pago_muni;
-					while(saldo_muni>0 && resultGastosMuni.next()){
+
+					while (saldo_muni > 0 && resultGastosMuni.next()) {
+						Double temp_saldo = saldo_muni;
+
 						gasto_muni = resultGastosMuni.getDouble("monto_renglon");
-							writer.println(String.join("","     Entidad: ",resultGastosMuni.getString("entidad_nombre"),", ",resultGastosMuni.getString("unidad_ejecutora_nombre")));
-							writer.println(String.join("","     ",resultGastosMuni.getString("municipio_nombre"),", ",resultGastosMuni.getString("departamento_nombre")));
-							writer.println(String.join("","     ", resultGastosMuni.getString("renglon_nombre")+", Q ",gasto_muni.toString()));
-							saldo_muni = saldo_muni - gasto_muni;
-							if(show_pagos)
-								writer.println("saldo_muni: "+saldo_muni);
-							writer.println();
-							while(saldo_muni>0 && resultGastosMuni.next()){
-								gasto_muni = resultGastosMuni.getDouble("monto_renglon");
-								writer.println(String.join("","     Entidad: ",resultGastosMuni.getString("entidad_nombre"),", ",resultGastosMuni.getString("unidad_ejecutora_nombre")));
-								writer.println(String.join("","     ",resultGastosMuni.getString("municipio_nombre"),", ",resultGastosMuni.getString("departamento_nombre")));
-								writer.println(String.join("","     ", resultGastosMuni.getString("renglon_nombre")+", Q ",gasto_muni.toString()));
-								saldo_muni = saldo_muni - gasto_muni;
-								if(show_pagos)
-									writer.println("saldo_muni: "+saldo_muni);
-								writer.println();
-							}
+
+						saldo_muni = saldo_muni - gasto_muni;
+
+						if (saldo_muni > 0) {
+							temp_saldo = gasto_muni;
+						}
+
+						agregarIscvGasto(dbpago_gastos, resultpago, resultGastosMuni, temp_saldo, "MU");
+
 					}
-					if(saldo_fondo<0 && !resultGastosFondo.isAfterLast()){
-						writer.println(String.join("","     Entidad: ",resultGastosFondo.getString("entidad_nombre"),", ",resultGastosFondo.getString("unidad_ejecutora_nombre")));
-						writer.println(String.join("","     ",resultGastosFondo.getString("municipio_nombre"),", ",resultGastosFondo.getString("departamento_nombre")));
-						writer.println(String.join("","     ", resultGastosFondo.getString("renglon_nombre")+", Q ",gasto_fondo.toString()));
+
+					if (saldo_fondo < 0 && !resultGastosFondo.isAfterLast()) {
+						Double temp_saldo = saldo_fondo;
+
 						saldo_fondo = saldo_fondo + pago_fondo;
-						if(show_pagos)
-							writer.println("saldo_fondo: "+saldo_fondo);
-						writer.println();
-					}
-					else
+
+						if (saldo_fondo < 0) {
+							temp_saldo = pago_fondo;
+						}
+
+						agregarIscvGasto(dbpago_gastos, resultpago, resultGastosFondo, temp_saldo, "FC");
+					} else
 						saldo_fondo = pago_fondo;
-					while(saldo_fondo>0 && resultGastosFondo.next()){
+
+					while (saldo_fondo > 0 && resultGastosFondo.next()) {
 						gasto_fondo = resultGastosFondo.getDouble("monto_renglon");
-							writer.println(String.join("","     Entidad: ",resultGastosFondo.getString("entidad_nombre"),", ",resultGastosFondo.getString("unidad_ejecutora_nombre")));
-							writer.println(String.join("","     ",resultGastosFondo.getString("municipio_nombre"),", ",resultGastosFondo.getString("departamento_nombre")));
-							writer.println(String.join("","     ", resultGastosFondo.getString("renglon_nombre")+", Q ",gasto_fondo.toString()));
-							saldo_fondo = saldo_fondo - gasto_fondo;
-							if(show_pagos)
-								writer.println("saldo_fondo: "+saldo_fondo);
-							writer.println();
-							while(saldo_fondo>0 && resultGastosFondo.next()){
-								gasto_fondo = resultGastosFondo.getDouble("monto_renglon");
-								writer.println(String.join("","     Entidad: ",resultGastosFondo.getString("entidad_nombre"),", ",resultGastosFondo.getString("unidad_ejecutora_nombre")));
-								writer.println(String.join("","     ",resultGastosFondo.getString("municipio_nombre"),", ",resultGastosFondo.getString("departamento_nombre")));
-								writer.println(String.join("","     ", resultGastosFondo.getString("renglon_nombre")+", Q ",gasto_fondo.toString()));
-								saldo_fondo = saldo_fondo - gasto_fondo;
-								if(show_pagos)
-									writer.println("saldo_fondo: "+saldo_fondo);
-								writer.println();
-							}
-						
+
+						Double temp_saldo = saldo_fondo;
+
+						saldo_fondo = saldo_fondo - gasto_fondo;
+
+						if (saldo_fondo > 0) {
+							temp_saldo = gasto_fondo;
+						}
+
+						agregarIscvGasto(dbpago_gastos, resultpago, resultGastosFondo, temp_saldo, "FC");
+
 					}
+
 					cont++;
-					if(cont%1000==0)
-						writer.flush();
+					if (cont % 1000 == 0) {
+						writer.println("SE HAN INSERTADO: " + (cont / 1000) + " MIL");
+
+					}
 				}
-				
-				return true;
+
+				writer.println("...FINALIZADO");
+
+				writer.flush();
 			}
 		} catch (Exception e) {
 			CLogger.writeFullConsole("ERROR-EXECUTION: liquidarPresupuesto(): ", e);
@@ -121,6 +122,9 @@ public class CLiquidacionPresupuestaria {
 				dbGastosMuni.close();
 				dbGastosFondo.close();
 				dbpago_munis.close();
+				dbpago_gastos.close();
+				
+				return true;
 			} catch (SQLException e) {
 				CLogger.writeFullConsole("ERROR-CLOSE: liquidarPresupuesto(): ", e);
 			}
@@ -128,6 +132,31 @@ public class CLiquidacionPresupuestaria {
 		}
 
 		return false;
+	}
+
+	private static void agregarIscvGasto(CDatabase dbpago_gastos, ResultSet rsISCV, ResultSet rsGastos, Double pago,
+			String tipo) throws SQLException {
+
+		dbpago_gastos.addBatch(rsISCV.getDouble("formulario"), rsISCV.getDouble("documento"), rsISCV.getDouble("cod_municipio"),
+				rsGastos.getDouble("no_cur"), rsGastos.getDouble("entidad"), rsGastos.getDouble("unidad_ejecutora"),
+				rsGastos.getDouble("programa"), rsGastos.getDouble("subprograma"), rsGastos.getDouble("proyecto"),
+				rsGastos.getDouble("actividad"), rsGastos.getDouble("obra"), rsGastos.getDouble("renglon"),
+				rsGastos.getDouble("geografico"), pago < 0 ? pago * -1 : pago, tipo);
+
+		// writer.println(String.join(""," Entidad:
+		// ",rsGastos.getString("entidad_nombre"),",
+		// ",rsGastos.getString("unidad_ejecutora_nombre")));
+		// writer.println(String.join("","
+		// ",rsGastos.getString("municipio_nombre"),",
+		// ",rsGastos.getString("departamento_nombre")));
+		// writer.println(String.join(""," ",
+		// rsGastos.getString("renglon_nombre")+", Q
+		// ",gasto_muni.toString()));
+
+		// if(show_pagos)
+		// writer.println("saldo_muni: "+saldo_muni);
+		// writer.println();
+
 	}
 
 }
